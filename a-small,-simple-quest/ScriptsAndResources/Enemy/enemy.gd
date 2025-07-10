02b8da4db0ce151e03479c18bdc358e3af1d005c
Bruno Amá Stephan <brunoastephan@gmail.com>
2025-07-10 16:03:36 -0300

    [feat] makes enemy attack intermittently
    Makes Enemy attack intermittently and it does affect Player health already.

--- a-small,-simple-quest/ScriptsAndResources/Enemy/enemy.gd
+++ a-small,-simple-quest/ScriptsAndResources/Enemy/enemy.gd
@@ -4,15 +4,34 @@ extends Damageable
 @export var resource : EnemyResource
 @onready var health_bar = $HealthBar/ProgressBar
 @onready var health_text = $HealthBar/Label
+@onready var attack_timer = $AttackTimer
+
+var player_ref : Player
 
 func _ready():
 	if resource:
 		max_health = resource.max_health
 		current_health = max_health
 		update_health_display()
+		attack_timer.wait_time = resource.attack_cooldown
+		attack_timer.start()
 	
-	# Connect health changed signal
 	health_changed.connect(_on_health_changed)
+	attack_timer.timeout.connect(_attempt_attack)
+
+func _attempt_attack():
+	if not is_instance_valid(player_ref):
+		# Find player if we don't have a reference
+		player_ref = get_tree().get_first_node_in_group("player")
+		if not player_ref: return
+	
+	if randf() <= resource.attack_chance:
+		_attack_player()
+
+func _attack_player():
+	print(resource.display_name, " attacks!")
+	player_ref.take_damage(resource.attack_damage)
+	# Add visual attack animation here later
 
 func _on_health_changed(_new_health, _max_health):
 	update_health_display()
--- a-small,-simple-quest/ScriptsAndResources/Enemy/enemy_resource.gd
+++ a-small,-simple-quest/ScriptsAndResources/Enemy/enemy_resource.gd
@@ -1,4 +1,3 @@
-# enemy_resource.gd
 class_name EnemyResource
 extends Resource
 
@@ -6,3 +5,5 @@ extends Resource
 @export var max_health := 50
 @export var attack_damage := 8
 @export var defense_value := 3
+@export var attack_cooldown := 3.0  # Seconds between attacks
+@export var attack_chance := 0.7    # 70% chance to attack when cooldown ends
